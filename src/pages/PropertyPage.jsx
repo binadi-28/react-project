49c992db78307e5cdce01956e956b75e3a7f24db
Binadi Laknara <binadi.20240153@iit.ac.lk>
2025-12-30 16:00:00 +0530

    eight commit

--- src/pages/PropertyPage.jsx
+++ src/pages/PropertyPage.jsx
@@ -1,9 +1,37 @@
 import { useParams } from "react-router-dom";
+import propertiesData from "../data/properties.json";
+import ImageGallery from "../components/ImageGallery";
+import TabsSection from "../components/TabsSection";
 
 function PropertyPage() {
   const { id } = useParams();
-  return <h2>Property Page: {id}</h2>;
+
+  const property = propertiesData.properties.find(
+    (p) => p.id === id
+  );
+
+  if (!property) {
+    return <p>Property not found</p>;
+  }
+
+  return (
+    <div>
+      <h2>
+        £{property.price.toLocaleString()} – {property.type}
+      </h2>
+      <p>{property.bedrooms} bedrooms · {property.postcode}</p>
+
+      {/* Image Gallery */}
+      <ImageGallery images={property.images} />
+
+      {/* Tabs */}
+      <TabsSection
+        description={property.description}
+        floorPlan={property.floorPlan}
+        mapUrl={property.mapUrl}
+      />
+    </div>
+  );
 }
 
 export default PropertyPage;
-
